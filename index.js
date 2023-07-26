85dbf78800d4e7da39087ed801351d11fdaaeb8d
SidneyKing <sid8121@gmail.com>
2023-07-26 18:15:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names,eventName){
+    let messages = []
+for (let k = 0; k < names.length; k ++){
+   messages.push(`Thank you, ${names[k]}, for the wonderful ${eventName} gift!`)
+}
+return messages
+}
+function countDown(){
+    let countDown = 0
+        while (countDown < 11){
+            console.log(countDown++)
+        } 
+    
+}
